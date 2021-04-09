39f4ad6e679c0f27326f60606815855366968fb1
Quang Ly <quangdly@gmail.com>
2021-04-09 10:25:21 -0400

    added java source files in the works

---
+++ src/com/company/DemoWithArguments.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class DemoWithArguments {
+
+    public static void main(String[] args)
+    {
+        for(String s:args)
+        {
+            System.out.println(s);
+        }
+    }
+}
